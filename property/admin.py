6edf9171594c71f655387dd3b57299d96bf1ae68
statik2002 <statik2002@mail.ru>
2022-10-25 17:02:56 +0800

    etup Flat changelist page.

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,9 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
 
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_editable = ('new_building',)
+
     search_fields = ('town', 'address', 'owner')
 
     readonly_fields = ('created_at', )
