005c0d10b529f1c04724e127aed61927a6132c27
statik2002 <statik2002@mail.ru>
2022-11-03 12:55:44 +0800

    высушил название поля

--- property/models.py
+++ property/models.py
@@ -58,7 +58,7 @@ class Flat(models.Model):
 class Complaint(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жалуется', related_name='complaints')
     flat = models.ForeignKey('Flat', on_delete=models.CASCADE, verbose_name='Квартира на которую жалуются', related_name='complaints')
-    complaint_text = models.TextField('Текст жалобы')
+    complaint = models.TextField('Текст жалобы')
 
     class Meta:
         verbose_name = 'Жалоба'
